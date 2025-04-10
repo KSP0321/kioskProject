51d1b9deb8e2127b6f63b5e0f4496247ea2763c7
spko <104554997+KSP0321@users.noreply.github.com>
2025-04-10 11:52:05 +0900

    Add files via upload

---
+++ kiosk-java/kiosk/src/java/com/example/lv7/Cart.java
@@ -0,0 +1,13 @@
+package java.com.example.lv7;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+
+    private final Map<MenuItem, Integer> cartItems = new HashMap<>();
+
+    public Map<MenuItem, Integer> getCartItems() {
+        return cartItems;
+    }
+}
